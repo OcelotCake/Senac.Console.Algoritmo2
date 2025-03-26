0623cc82acbd66275e265981209199d4573313b9
CHARLESKUNDEVITALE <CHARLESKUNDEVITALE7@gmail.com>
2025-03-25 21:50:22 -0300

    Leitura de variaveies

--- Algoritimo2/Algoritimo2/Program.cs
+++ Algoritimo2/Algoritimo2/Program.cs
@@ -4,3 +4,18 @@ decimal preco = 20M;
 char letra = 'a';
 var nomeCompleto = "Charles Kunde Vitale";
 var altura = 180;
+var msg = " thank You";
+
+Console.Write("Digite seu nome: ");
+nomeCompleto = Console.ReadLine();
+Console.WriteLine();
+Console.WriteLine("Bem Vindo " + nomeCompleto);
+Console.WriteLine();
+Console.Write("Digite Sua idade: ");
+var linha = Console.ReadLine();
+idade = int.Parse(linha);
+Console.WriteLine("Idade informada " + idade  + msg);
+if (idade < 18)
+    Console.WriteLine("Voce nao pode estar aqui!");
+
+Console.ReadKey();
